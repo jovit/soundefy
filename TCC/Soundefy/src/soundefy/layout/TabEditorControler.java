45dec93db0126d8c070b8969c47b3859f4d75f2c
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2015-08-11 22:28:02 -0300

    draw lines and time signiature on the tab editor

--- TCC/Soundefy/src/soundefy/layout/TabEditorControler.java
+++ TCC/Soundefy/src/soundefy/layout/TabEditorControler.java
@@ -5,11 +5,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import model.Tab;
+import javafx.scene.text.Font;
 
 public class TabEditorControler {
 	public static final int MARGIN = 20;
-	private Tab tab;
+	public static final int LINE_SPACING = 10;
+	public static final double LINE_WIDTH = 1;
 
 	@FXML
 	private Canvas canvas;
@@ -34,16 +35,43 @@ public class TabEditorControler {
 		canvas.heightProperty().addListener(observable -> drawTab());
 		
 		canvas.widthProperty().bind(parent.widthProperty().subtract(MARGIN));
-		canvas.heightProperty().bind(parent.heightProperty());
+		canvas.heightProperty().bind(parent.heightProperty().subtract(MARGIN));
 		
-		drawTab();
 		
+	}
+	
+	private void drawTimeSigniature(int a, int b, int y){
+		context.setStroke(Color.BLACK);
+		context.setFill(Color.BLACK);
+		context.setFont(new Font("Arial", 20));
+		context.fillText(String.valueOf(a), MARGIN + 5, y+20);
+		context.fillText(String.valueOf(b), MARGIN + 5, y+40);
+	}
+	
+	private int drawLines(int y){
+		context.setLineWidth(LINE_WIDTH);
+		context.setStroke(Color.gray(0.6));
+		context.strokeLine(MARGIN, y, MARGIN, y+ 5*LINE_SPACING);
+		context.strokeLine(canvas.getWidth() - 1, y, canvas.getWidth() - 1, y + 5*LINE_SPACING);
+
+		for(int i=0; i<6; i++){
+			context.strokeLine(MARGIN, y, canvas.getWidth(), y);
+			y += LINE_SPACING; 
+		}
+		
+		return y + MARGIN;
 	}
 
 	private void drawTab() {
-		
-		context.setFill(Color.BLACK);
+		int where = MARGIN;
+		context.setFill(Color.WHITE);
 		context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		where = drawLines(where);
+		drawTimeSigniature(4,4,MARGIN);
+		
+		drawLines(where);
+		drawTimeSigniature(4,4,where);
 
 	}
 
