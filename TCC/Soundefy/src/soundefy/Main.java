81d61b0ed34f166059a064b32f878e1a1c03e77b
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2015-10-31 16:07:23 -0200

    TabBrowser fxml created

--- TCC/Soundefy/src/soundefy/Main.java
+++ TCC/Soundefy/src/soundefy/Main.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import soundefy.layout.TabEditorController;
 import soundefy.model.TimeSignature;
@@ -19,10 +18,10 @@ public class Main extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Soundefy");
-		
+
 		openLogin();
 	}
-	
+
 	public void openTab() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("layout/TabEditor.fxml"));
@@ -33,10 +32,10 @@ public class Main extends Application{
 		controller.setPrimaryStage(this.primaryStage);
 		this.primaryStage.setScene(new Scene(pane));
 		this.primaryStage.show();
-		
+
 		new SoundefyClient();
 	}
-	
+
 	public void openLogin() throws IOException{
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("layout/Login.fxml"));
@@ -45,7 +44,7 @@ public class Main extends Application{
 		this.primaryStage.setScene(new Scene(pane));
 		this.primaryStage.show();
 	}
-	
+
 	public static void main(String args[]){
 		launch(args);
 	}
--- TCC/Soundefy/src/soundefy/util/Server.java
+++ TCC/Soundefy/src/soundefy/util/Server.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Server {
-	private server_main.Server server;
+	/*private server_main.Server server;
 
 	public Server() {
 		server = new server_main.Server();
@@ -15,5 +15,5 @@ public class Server {
 		int result = -1;
 		Socket socket = new Socket("localhost", server_main.Server.PORT);
 		return result;
-	}
+	}*/
 }
